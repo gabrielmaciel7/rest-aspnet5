65c0e1231e89509558c94b13ebd316cf03d31908
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-20 15:03:03 -0300

    add person service interface

---
+++ RestWithASPNET5/RestWithASPNET5/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET5.Models;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
