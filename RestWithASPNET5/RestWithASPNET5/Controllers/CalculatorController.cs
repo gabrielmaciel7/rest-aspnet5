d07612a2d039bea4708cf153291a71ec1b14f9ea
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-20 14:46:15 -0300

    add sum on calculator controller

---
+++ RestWithASPNET5/RestWithASPNET5/Controllers/CalculatorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input.");
+        }
+
+        private static bool IsNumeric(string strNumber)
+        {
+            var isNumber = double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out double number);
+
+            return isNumber;
+        }
+
+        private static decimal ConvertToDecimal(string strNumber)
+        {
+
+            if (decimal.TryParse(strNumber, out decimal decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+    }
+}
