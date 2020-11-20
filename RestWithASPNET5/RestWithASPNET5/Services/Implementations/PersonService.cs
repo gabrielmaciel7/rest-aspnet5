ea3240a271cd98d79987e49f0f466c04671178d5
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-20 15:17:40 -0300

    add person service

---
+++ RestWithASPNET5/RestWithASPNET5/Services/Implementations/PersonService.cs
@@ -0,0 +1,72 @@
+﻿using RestWithASPNET5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+
+        }
+
+        public List<Person> FindAll()
+        {
+            var people = new List<Person>();
+
+            for (var i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+
+                people.Add(person);
+            }
+
+            return people;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Gabriel",
+                LastName = "Soares",
+                Address = "Address, 555",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = $"Name {i}",
+                LastName = $"LastName {i}",
+                Address = $"Address, {i * 100}",
+                Gender = i % 2 == 0? "Male" : "Female"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
