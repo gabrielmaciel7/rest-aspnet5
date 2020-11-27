e2d8f24f24cde32caae388eb3e17f8bfb5543c62
Gabriel Maciel <gabrielmaciel.nave@gmail.com>
2020-11-27 11:07:32 -0300

    [Controllers] Add BooksController

---
+++ RestWithASPNET5/RestWithASPNET5/Controllers/BooksController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET5.Services;
+using RestWithASPNET5.Models;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+        private readonly IBooksService _bookService;
+
+        public BooksController(ILogger<BooksController> logger, IBooksService bookService)
+        {
+            _logger = logger;
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_bookService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Show(long id)
+        {
+            var book = _bookService.FindById(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Books book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_bookService.Create(book));
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Books book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_bookService.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _bookService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
