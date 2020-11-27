6fa199c6d4cfcbc9a50dcd00c7edb8a2ee25c705
Gabriel Soares <gsoares@vitait.com>
2020-11-27 11:59:06 -0300

    [Entities] Add Base entity

---
+++ RestWithASPNET5/RestWithASPNET5/Entities/Base.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET5.Entities
+{
+    public class Base
+    {
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
