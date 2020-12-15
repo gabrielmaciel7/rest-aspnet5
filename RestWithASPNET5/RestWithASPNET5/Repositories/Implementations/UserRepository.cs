31efa0a9949e619462a9e428a8794bea9dcc6e96
Gabriel Soares <gsoares@vitait.com>
2020-12-15 09:52:57 -0300

    [UserRepository] Config User Token

---
+++ RestWithASPNET5/RestWithASPNET5/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using RestWithASPNET5.Data.VO;
+using RestWithASPNET5.Entities;
+using RestWithASPNET5.Models.Context;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASPNET5.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidateCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (_context.Users.Any(u => u.Id.Equals(user.Id))) return null;
+
+            var result = _context.Users.SingleOrDefault(x => x.Id.Equals(user.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return result;
+        }
+
+        private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
---
+++ RestWithASPNET5/RestWithASPNET5/Repositories/models/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASPNET5.Data.VO;
+using RestWithASPNET5.Entities;
+
+namespace RestWithASPNET5.Repositories
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+    }
+}
