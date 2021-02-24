3a8e8486e9a3e95ed6b4fea0313478859e4b7801
Gabriel Soares <gsoares@vitait.com>
2021-02-24 09:18:55 -0300

    [TokenService] Add GenerateAccessToken method

---
+++ RestWithASPNET5/RestWithASPNET5/Services/implementations/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RestWithASPNET5.Configurations;
+using RestWithASPNET5.Services.models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Services.implementations
+{
+    public class TokenService : ITokenService
+    {
+        private readonly TokenConfiguration _configuration;
+
+        public TokenService(TokenConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenOptions = new JwtSecurityToken
+                (issuer: _configuration.Issuer, audience: _configuration.Audience, claims: claims,
+                    expires: DateTime.Now.AddMinutes(_configuration.Minutes), signingCredentials: signinCredentials);
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            return tokenString;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
