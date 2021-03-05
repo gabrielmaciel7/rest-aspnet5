d071bc440933cffd7053bc36a0e58d7f20b59fc6
Gabriel Soares <gsoares@vitait.com>
2021-03-05 11:42:08 -0300

    [GenericRepository] Add FindWithPagedSearch method

--- RestWithASPNET5/RestWithASPNET5/Repositories/Implementations/GenericRepository.cs
+++ RestWithASPNET5/RestWithASPNET5/Repositories/Implementations/GenericRepository.cs
@@ -89,5 +89,26 @@ namespace RestWithASPNET5.Repositories.Implementations
         {
             return _dataset.Any(item => item.Id.Equals(id));
         }
+
+        public List<T> FindWithPagedSearch(string query)
+        {
+            return _dataset.FromSqlRaw<T>(query).ToList();
+        }
+
+        public int GetCount(string query)
+        {
+            var result = "";
+
+            using (var connection = _context.Database.GetDbConnection())
+            {
+                connection.Open();
+
+                using var command = connection.CreateCommand();
+                command.CommandText = query;
+                result = command.ExecuteScalar().ToString();
+            }
+
+            return int.Parse(result);
+        }
     }
 }
--- RestWithASPNET5/RestWithASPNET5/Repositories/models/IGenericRepository.cs
+++ RestWithASPNET5/RestWithASPNET5/Repositories/models/IGenericRepository.cs
@@ -8,9 +8,12 @@ namespace RestWithASPNET5.Repositories
         T Create(T item);
         T FindById(long id);
         List<T> FindAll();
+        List<T> FindWithPagedSearch(string query);
         T Update(T item);
         void Delete(long id);
+
         bool Exists(long id);
+        int GetCount(string query);
 
     }
 }
